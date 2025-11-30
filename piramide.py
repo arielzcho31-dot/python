49f416651c289af38c66647e8107ce02cb6b231f
arielzcho <cesarzaracho2003@gmail.com>
2025-11-30 18:51:53 -0300

    Revert "primer cambio"
    This reverts commit fbe6f020ba47e229525fb3ad57053150462efb86.

--- piramide.py
+++
@@ -1,8 +0,0 @@
-#ingresar la cantidad de asteriscos para la pirámide y imprimir la altura
-
-bloques = int(input("Introduce la cantidad de bloques para la pirámide: ")) 
-altura = 0
-while bloques >= altura + 1:
-    altura += 1
-    bloques -= altura
-print("La altura de la pirámide es:", altura)
